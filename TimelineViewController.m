832650c8bcf2dc77214f0e765c54f7b5e64f2dda
omerhakanbilici <omerhakanbilici@gmail.com>
2013-12-10 19:30:05 +0200

    code review.

--- TimelineViewController.m
+++ TimelineViewController.m
@@ -41,11 +41,6 @@
     }
     else NSLog(@"Proximity Monitoring Not-Enabled.");
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 //Can't work with simulator. Poor man's developer.
