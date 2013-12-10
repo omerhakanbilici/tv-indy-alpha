4bb74e31e136e56d7ad87976945263af5a5c76dc
omerhakanbilici <omerhakanbilici@gmail.com>
2013-12-10 19:28:50 +0200

    code review.

--- TimelineViewController.m
+++ TimelineViewController.m
@@ -46,7 +46,6 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-    NSLog(@"hop");
 }
 
 //Can't work with simulator. Poor man's developer.
